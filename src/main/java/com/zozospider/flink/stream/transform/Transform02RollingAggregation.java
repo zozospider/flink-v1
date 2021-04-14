d9350a33cd5becaf1e025412df0364e99c3b23b9
zozospider <2642220896@qq.com>
2021-04-14 17:26:17 +0800

    update flink-v1

---
+++ src/main/java/com/zozospider/flink/stream/transform/Transform02RollingAggregation.java
@@ -0,0 +1,55 @@
+package com.zozospider.flink.stream.transform;
+
+import com.zozospider.flink.beans.SensorReading;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+// 转换算子 - 滚动聚合算子 - keyBy() & sum() min() max() minBy() maxBy()
+public class Transform02RollingAggregation {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment streamEnv = StreamExecutionEnvironment.getExecutionEnvironment();
+        streamEnv.setParallelism(1);
+
+        // keyBy():
+        // 先通过 keyBy() 得到 KeyedStream 后才能做滚动聚合操作, 如 sum() min() 等
+
+        DataStreamSource<String> dataStream = streamEnv.readTextFile("data-dir/sensor.txt");
+
+        /*SingleOutputStreamOperator<SensorReading> dataStream2 = dataStream.map(new MapFunction<String, SensorReading>() {
+            @Override
+            public SensorReading map(String s) throws Exception {
+                String[] fields = s.split(",");
+                return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2]));
+            }
+        });*/
+        SingleOutputStreamOperator<SensorReading> dataStream2 = dataStream.map((String s) -> {
+            String[] fields = s.split(",");
+            return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2]));
+        });
+
+        // keyBy() 分组
+        // KeyedStream<SensorReading, Tuple> dataStream3 = dataStream2.keyBy("id");
+        /*KeyedStream<SensorReading, String> dataStream3 = dataStream2.keyBy(new KeySelector<SensorReading, String>() {
+            @Override
+            public String getKey(SensorReading sensorReading) throws Exception {
+                return sensorReading.getId();
+            }
+        });*/
+        /*KeyedStream<SensorReading, String> dataStream3 = dataStream2.keyBy((SensorReading sensorReading) ->
+                sensorReading.getId());*/
+        KeyedStream<SensorReading, String> dataStream3 = dataStream2.keyBy(SensorReading::getId);
+
+        // 滚动聚合
+        // 求最大的温度值
+        SingleOutputStreamOperator<SensorReading> dataStreamA = dataStream3.max("temperature");
+        SingleOutputStreamOperator<SensorReading> dataStreamB = dataStream3.maxBy("temperature");
+        dataStreamA.print("dataStreamA");
+        dataStreamB.print("dataStreamB");
+
+        streamEnv.execute("Transform");
+    }
+
+}
---
+++ src/main/java/com/zozospider/flink/stream/transform/Transform03Reduce.java
@@ -0,0 +1,51 @@
+package com.zozospider.flink.stream.transform;
+
+import com.zozospider.flink.beans.SensorReading;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+// 转换算子 - 聚合算子 - keyBy() & reduce()
+public class Transform03Reduce {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment streamEnv = StreamExecutionEnvironment.getExecutionEnvironment();
+        streamEnv.setParallelism(1);
+
+        // keyBy():
+        // 先通过 keyBy() 得到 KeyedStream 后才能做聚合操作, 如 reduce()
+
+        DataStreamSource<String> dataStream = streamEnv.readTextFile("data-dir/sensor.txt");
+
+        SingleOutputStreamOperator<SensorReading> dataStream2 = dataStream.map((String s) -> {
+            String[] fields = s.split(",");
+            return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2]));
+        });
+
+        // keyBy() 分组
+        KeyedStream<SensorReading, String> dataStream3 = dataStream2.keyBy(SensorReading::getId);
+
+        // reduce() 聚合
+        // 求最大的温度值, 时间戳用当前数据的
+        /*SingleOutputStreamOperator<SensorReading> dataStream4 = dataStream3.reduce(new ReduceFunction<SensorReading>() {
+            @Override
+            public SensorReading reduce(SensorReading sensorReading1, SensorReading sensorReading2) throws Exception {
+                return new SensorReading(
+                        sensorReading1.getId(),
+                        sensorReading2.getTimestamp(),
+                        Math.max(sensorReading1.getTemperature(), sensorReading2.getTemperature()));
+            }
+        });*/
+        SingleOutputStreamOperator<SensorReading> dataStream4 = dataStream3.reduce((SensorReading value1, SensorReading value2) ->
+                new SensorReading(
+                        value1.getId(),
+                        value2.getTimestamp(),
+                        Math.max(value1.getTemperature(), value2.getTemperature()))
+        );
+        dataStream4.print();
+
+        streamEnv.execute("Transform");
+    }
+
+}
